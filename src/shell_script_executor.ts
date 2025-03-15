34df8ede98e685bd2f16c0376d3fe87940c431eb
ysnows <ysnows@163.com>
2025-03-15 10:46:10 +0800

    update path

--- src/shell_script_executor.ts
+++ src/shell_script_executor.ts
@@ -1,18 +1,18 @@
 import { Response, Action, RequestOptions, res, getPythonEnv, getProjectEnv } from '@enconvo/api';
-import { execSync, spawn } from 'child_process';
+import { spawn } from 'child_process';
 import fs from "fs"
-import { homedir } from 'os';
-import { getArchitecture } from './utils/arch_util.ts';
 
 interface Options extends RequestOptions {
     shell_script: string,
     args: string,
     cwd?: string,
+    need_run_in_background?: boolean
+    background_timeout?: number
 }
 
 export default async function main(request: Request): Promise<Response> {
     const options: Options = await request.json();
-    console.log("sessionId", options.session_id)
+    console.log("need_run_in_background", options.need_run_in_background, options.background_timeout)
 
     let shell_script = options.shell_script
     if (!shell_script || shell_script.length <= 0) {
@@ -52,60 +52,57 @@ export default async function main(request: Request): Promise<Response> {
     /**
      * write shell script
      */
-    const shellFilePath = `${projectPath}/temp_script.sh`;
+    const shellFilePath = `${projectPath}/.temp_script.sh`;
     fs.writeFileSync(shellFilePath, newCode);
-
-    /**
-     * set shell
-     */
-    const shell = process.env.SHELL || '/bin/bash'
-
-    /**
-     * set node path
-     */
-    let exportPath = 'export PATH="/sbin/:$PATH" &&';
-    try {
-        execSync('node -v', { stdio: 'ignore' });
-    } catch (error) {
-        const arch = getArchitecture()
-        const nodePath = `${homedir()}/.config/enconvo/preload/node/${arch}/bin`
-        process.env.PATH = `${nodePath}:${process.env.PATH}`
-    }
-
-
-    const command = `${exportPath} ${sourceVenv} ${shell} '${shellFilePath}' ${args}`;
-    console.log('command', command);
-
-    const child = spawn("/bin/bash", ['-c', command], {
-        cwd: projectPath,
-        env: process.env
-    });
-
-    let output = '';
-    child.stdout.on('data', async (data) => {
-        const chunk = data.toString();
-        console.log("data:", chunk);
-        output += chunk;
-
-        res.write({
-            content: chunk,
-            action: res.WriteAction.AppendToLastMessageLastTextContent
-        })
-    });
-
-    child.stderr.on('data', (data) => {
-        const chunk = data.toString();
-        console.log("error data:", chunk);
-        res.write({
-            content: chunk,
-            action: res.WriteAction.AppendToLastMessageLastTextContent
-        })
-        console.error(chunk);
-        output += chunk;
-    });
-
-
     const result = await new Promise<{ code: number, output: string }>((resolve, reject) => {
+        /**
+         * set shell
+         */
+        const shell = process.env.SHELL || '/bin/bash'
+
+
+        const command = `${sourceVenv} ${shell} '${shellFilePath}' ${args}`;
+        console.log('command', command);
+
+        const child = spawn("/bin/bash", ['-c', command], {
+            cwd: projectPath,
+            env: process.env
+        });
+
+        let output = '';
+        let timer: NodeJS.Timeout | null = null;
+        child.stdout.on('data', async (data) => {
+            timer && clearTimeout(timer);
+            const chunk = data.toString();
+            console.log("data:", chunk);
+            output += chunk;
+            if (options.need_run_in_background) {
+                timer = setTimeout(() => {
+                    console.log('run success');
+                    resolve({
+                        code: 0,
+                        output
+                    });
+                }, options.background_timeout || 1000 * 10);
+            }
+
+            res.write({
+                content: chunk,
+                action: res.WriteAction.AppendToLastMessageLastTextContent
+            })
+        });
+
+        child.stderr.on('data', (data) => {
+            const chunk = data.toString();
+            console.log("error data:", chunk);
+            res.write({
+                content: chunk,
+                action: res.WriteAction.AppendToLastMessageLastTextContent
+            })
+            console.error(chunk);
+            output += chunk;
+        });
+
         child.on('close', (code) => {
             resolve({
                 code: code || 0,
